dcfe8363695277aba8aaa980d60e99e1d9978c2a
unknown <11Rahulsinghrawat@gmail.com>
2025-01-24 16:11:50 +0530

    updated admin dashboard

--- src/Pages/AdminPage/PlayerData.jsx
+++ src/Pages/AdminPage/PlayerData.jsx
@@ -45,7 +45,8 @@ const PlayerData = () => {
             <table class="table-fixed w-11/12 text-center border-2 mt-10">
                 <thead className='border-b-2 border-white'>
                     <tr className=' font-extrabold'>
-                        <th className='border-r-2 border-white truncate py-4'>UserId</th>
+                        <th className='border-r-2 border-white truncate py-4'>S.No</th>
+                        <th className='border-r-2 border-white truncate py-4'>PlayerId</th>
                         <th className='border-r-2 border-white truncate py-4'>Approved(Toggle)</th>
                         <th className='border-r-2 border-white truncate py-4'>Email</th>
                         <th className='border-r-2 border-white truncate py-4'>Name</th>
@@ -54,8 +55,9 @@ const PlayerData = () => {
                 </thead>
                 <tbody>
                     {
-                        getPlayer?.map((items) => (
+                        getPlayer?.map((items,index) => (
                             <tr className='border-b-2 border-white font-semibold'>
+                                <td className='border-r-2 border-white truncate py-4'>{index+1}</td>
                                 <td className='border-r-2 border-white truncate py-4'>{items?._id}</td>
                                 <td className={`border-r-2 border-white truncate py-4  cursor-pointer`} onClick={()=>playerStatus(items?._id)}><button className={`${items?.approved ? " bg-green-400" : "bg-red-600"} p-2 rounded-full shadow-yellow-100 w-1/2`}>{items?.approved ? "True" : "False"}</button></td>
                                 <td className='border-r-2 border-white truncate py-4'>{items?.email}</td>
